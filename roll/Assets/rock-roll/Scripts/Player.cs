807845506761fa98bb92377709c1410551ce5fa2
Jad2087 <142918288+Jad2087@users.noreply.github.com>
2025-11-25 14:14:47 -0500

    Update Player.cs

--- roll/Assets/rock-roll/Scripts/Player.cs
+++ roll/Assets/rock-roll/Scripts/Player.cs
@@ -2,65 +2,118 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using extOSC;
-
+ 
 public class Player : MonoBehaviour
 {
-    public OSCReceiver oscReceiver;
-
     public float torqueForce = 1f;
     public float jumpForce = 5f;
+ 
+    public float checkRadius = 0.1f;
     public LayerMask groundLayer;
-
+ 
     private Rigidbody2D rb;
-
+ 
+    public extOSC.OSCReceiver oscReceiver;
+    public extOSC.OSCTransmitter oscTransmitter;
+ 
+ 
+ 
     private int etatEnMemoire = 1;
-    private int valeurTorque = 0;
-
+    private int etatEnMemoire2 = 1;// Le code initalise l'état initial du bouton comme relâché
+ 
+      private void maLectureKey(OSCMessage message)
+    {
+ 
+        // Si le message n'a pas d'argument ou l'argument n'est pas un Int on l'ignore
+        if (message.Values.Count == 0)
+        {
+            Debug.Log("No value in OSC message");
+            return;
+        }
+ 
+        if (message.Values[0].Type != OSCValueType.Int)
+        {
+            Debug.Log("Value in message is not an Int");
+            return;
+           
+        }
+ 
+           int nouveauEtat = message.Values[0].IntValue; // REMPLACER ici les ... par le code qui permet de récuérer la nouvelle donnée du flux
+        if (etatEnMemoire != nouveauEtat)
+        { // Le code compare le nouvel etat avec l'etat en mémoire
+            etatEnMemoire = nouveauEtat; // Le code met à jour l'état mémorisé
+            if (nouveauEtat == 1)
+            {
+                if (IsGrounded())
+                {
+                    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+ 
+                        var oSCMessage = new OSCMessage("/color");  // CHANGER l'adresse /pixel pour l'adresse désirée
+                        oSCMessage.AddValue( OSCValue.Int(0) ); // Ajoute l'entier 255
+                        oSCMessage.AddValue( OSCValue.Int(0) ); // Ajoute un autre 255
+                        oSCMessage.AddValue( OSCValue.Int(0) ); // Ajoute un troisième 255
+                        oscTransmitter.Send(oSCMessage);
+                       
+                }  
+               
+               
+            }
+            else if (nouveauEtat == 0)
+            {
+                     var oSCMessage = new OSCMessage("/color");  // CHANGER l'adresse /pixel pour l'adresse désirée
+                        oSCMessage.AddValue( OSCValue.Int(250) ); // Ajoute l'entier 255
+                        oSCMessage.AddValue( OSCValue.Int(0) ); // Ajoute un autre 255
+                        oSCMessage.AddValue( OSCValue.Int(0) ); // Ajoute un troisième 255
+                        oscTransmitter.Send(oSCMessage);
+            }
+        }
+ 
+    }
+ 
+    private void maLectureAnalogique(OSCMessage message)
+    {
+ 
+        // Si le message n'a pas d'argument ou l'argument n'est pas un Int on l'ignore
+        if (message.Values.Count == 0)
+        {
+            Debug.Log("No value in OSC message");
+            return;
+        }
+ 
+        if (message.Values[0].Type != OSCValueType.Int)
+        {
+            Debug.Log("Value in message is not an Int");
+            return;
+        }
+ 
+ 
+        int AngleValue = message.Values[0].IntValue; // REMPLACER ici les ... par le code qui permet de récuérer la nouvelle donnée du flux
+ 
+            if (AngleValue > 0)
+            {
+                rb.AddTorque(-torqueForce); // clockwise
+           
+            }
+            else if (AngleValue < 0)
+            {
+                rb.AddTorque(torqueForce); // counter-clockwise
+         
+            }
+ 
+    }
+ 
+ 
+ 
+ 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        // Bind OSC
-        oscReceiver.Bind("/changement", changementEncodeur);
-        oscReceiver.Bind("/saut", etatBouton);
+ 
+        oscReceiver.Bind("/but", maLectureKey);
+        oscReceiver.Bind("/angle", maLectureAnalogique);
+        if (oscReceiver == null) return;
     }
-
-    private void changementEncodeur(OSCMessage message)
-{
-
-    int AngleValue = message.Values[0].IntValue;
-
-    int value = AngleValue;
-    if (valeurTorque != value)
-    {
-        valeurTorque = value;
-        if (value < 0)
-        {
-            rb.AddTorque(torqueForce);
-                Debug.Log("ca tourne");
-            }
-        else if (value > 0)
-        {
-            rb.AddTorque(-torqueForce);
-                Debug.Log("ca tourne");
-            }
-    }
-}
-
-    private void etatBouton(OSCMessage message)
-    {
-        int nouveauEtat = message.Values[0].IntValue;
-
-        if (etatEnMemoire != nouveauEtat)
-        {
-            etatEnMemoire = nouveauEtat;
-
-            if (nouveauEtat == 0)
-            {
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            }
-        }
-    }
-
+ 
     void FixedUpdate()
     {
         // Roll left/right
@@ -72,14 +125,14 @@ public class Player : MonoBehaviour
         {
             rb.AddTorque(torqueForce); // counter-clockwise
         }
-
-
+ 
+       
     }
-
+ 
     void Update()
     {
-        // Jump
-        // GetKeyDown() does not work in FixedUpdate()
+         // Jump
+         // GetKeyDown() does not work in FixedUpdate()
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             if (IsGrounded())
@@ -88,7 +141,7 @@ public class Player : MonoBehaviour
             }
         }
     }
-
+ 
     public bool IsGrounded()
     {
         float extraHeight = 0.1f;
@@ -101,3 +154,4 @@ public class Player : MonoBehaviour
         return hit.collider != null;
     }
 }
+ 
